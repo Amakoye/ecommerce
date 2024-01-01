016473b3294fb082a50ea55a56333ab31426493b
Amakoye <charlesamakoye@gmail.com>
2024-01-01 19:33:35 +0300

    complete understanding django queries and build product filtering prototype

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -81,9 +81,17 @@ WSGI_APPLICATION = "ecommerce.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "127.0.0.1",
+        "PORT": "5434",
     }
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
 }
 
 
